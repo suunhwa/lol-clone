3979a885c6c1d685102a0e4df3ffec2d3a84335a
HYUNGJINSARI <daasaly@naver.com>
2026-04-21 17:56:00 +0900

    26.04.21
    1. 구조체 만들고있음

---
+++ Source/LeagueofLegends/public/Struct/ItemStruct.h
@@ -0,0 +1,120 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemStruct.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FItemBaseRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	FItemBaseRow() : Item_ID(0), Unique_ID(TEXT("")), Price(0), RefundPrice(0), Recipe(TEXT("")), 
+	Is_Active(0), Is_Consumable(0), Cooldown(0), Max_Stack(0), Name_KR(TEXT("")), Name_EN(TEXT("")) {}
+
+	// DataTable Yellow Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	int32 Item_ID;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	FString Unique_ID;
+	
+	// DataTable Blue Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	int32 Price;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	int32 RefundPrice;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	FString Recipe;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	bool Is_Active;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	bool Is_Consumable;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	float Cooldown;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	int32 Max_Stack;
+	
+	// DataTable Red Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	FString Name_KR;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Base")
+	FString Name_EN;
+};
+
+USTRUCT(BlueprintType)
+struct FItemStatRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	FItemStatRow() : Item_ID(0), Stat_Type(TEXT("")), Stat_Value(0.f), Description(TEXT("")) {}
+
+	// DataTable Yellow Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Stat")
+	int32 Item_ID;
+
+	// DataTable Red Section 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Stat")
+	FString Stat_Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Stat")
+	float Stat_Value;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Stat")
+	FString Description;
+};
+
+USTRUCT(BlueprintType)
+struct FItemEffectRow : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+public:
+	FItemEffectRow() : Effect_Name(TEXT("")), Stat_Type(TEXT("")), Value01(0.f), Value02(0.f), Description(TEXT("")) {}
+	
+	// DataTable Yellow Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	FString Effect_Name;
+	
+	// DataTable Red Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	FString Stat_Type;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	float Value01;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	float Value02;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effect")
+	FString Description;
+};
+
+USTRUCT(BlueprintType)
+struct FItemStatTypeRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	FItemStatTypeRow() : Category(TEXT("")), StatType(TEXT("")), DataType(TEXT("")) {}
+
+	// DataTable Red Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|StatType")
+	FString Category;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|StatType")
+	FString StatType;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|StatType")
+	FString DataType;
+};
