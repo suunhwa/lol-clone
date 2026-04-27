4d4c1b539cb4dcb4351f2eabfc9a0a812ef1a577
HYUNGJINSARI <daasaly@naver.com>
2026-04-27 16:48:44 +0900

    fix datatable, add minion stat component

--- Source/LeagueofLegends/public/Characters/LoLMinion.h
+++ Source/LeagueofLegends/public/Characters/LoLMinion.h
@@ -37,9 +37,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	AActor* TargetPlayer;
 	
-	// 이동속도 // 나중에 데이터테이블에서 가져오기 전까지 사용할 임시값
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
-	float MoveSpeed = 300.0f;
+	
 	
 	UPROPERTY(VisibleAnywhere, Category = "AI")
 	EMinionState CurrentState = EMinionState::MoveToTarget;
@@ -62,6 +60,10 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "AI|Stats")
 	float AttackRange = 150.0f; 
 
+	// 이동속도 // 나중에 데이터테이블에서 가져오기 전까지 사용할 임시값
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float MoveSpeed = 300.0f;
+	
 	UPROPERTY(EditAnywhere, Category = "AI|Stats")
 	float AttackSpeed = 1.0f; // 초당 공격 횟수
 
