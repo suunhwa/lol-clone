d5006eab7ffba8f0140695023fb6375523dfe350
tang-ka <gur0907@hanyang.ac.kr>
2026-05-06 14:58:39 +0900

    wip: 시야에 따라 렌더링 안할 오브젝트 구분

--- Source/LeagueofLegends/public/Interfaces/SightProvider.h
+++ Source/LeagueofLegends/public/Interfaces/SightProvider.h
@@ -29,6 +29,9 @@ class LEAGUEOFLEGENDS_API ISightProvider
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Sight")
+	bool
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Sight")
 	FVector GetSightOrigin() const;
 	
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Sight")
