3c26dfcc1b8f08ebaf5463c24ae06ad6cd9df00f
suunhwa <snna628@gmail.com>
2026-05-20 19:15:08 +0900

    fix: batch script

--- LeagueofLegends.bat
+++ LeagueofLegends.bat
@@ -1,1 +1,7 @@
-"C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Projects\POTENUP-LeagueofLegends\LeagueofLegends.uproject" -game -ResX=800 -ResY=600 -WinX=100 -WinY=100 -WINDOWED -log
+@echo off
+
+set EDITOR="C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor.exe"
+set PROJECT="C:\Projects\POTENUP-LeagueofLegends\LeagueofLegends.uproject"
+set MAP=/Game/Maps/Lv_Lobby
+
+start "League of Legends" %EDITOR% %PROJECT% %MAP%?listen -game -ResX=700 -ResY=580 -WinX=0 -WinY=0 -WINDOWED -log
